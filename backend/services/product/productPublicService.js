16d4c47ee28a686035a74164b6327bce9f1b21f3
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-12-05 06:43:58 +0000

    fix: correction import module public/index et ajout render.yaml pour déploiement

--- backend/services/product/productPublicService.js
+++ backend/services/product/productPublicService.js
@@ -1,5 +1,5 @@
 // Ce fichier a été réorganisé en sous-modules dans le dossier public/
 // Utilisez require('./public/index') pour importer les services
-const productPublicService = require('./public/index');
+const productPublicService = require('./public/index.js');
 
 module.exports = productPublicService;
