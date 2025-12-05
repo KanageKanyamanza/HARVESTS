13a42e4ac2ed68b3eab258bb03baf461b88752e4
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-12-05 06:55:33 +0000

    fix: ajout extension .js aux imports des services public pour Render

--- backend/services/product/productPublicService.js
+++ backend/services/product/productPublicService.js
@@ -1,9 +1,9 @@
 // Ce fichier a été réorganisé en sous-modules dans le dossier public/
 // Import direct des services pour éviter les problèmes de résolution de module
-const { getAllProducts, searchProducts } = require('./public/productSearchService');
-const { getProductsByLocation } = require('./public/productLocationService');
-const { getCategories, getProductsByCategory } = require('./public/productCategoryService');
-const { getProductById, getFeaturedProducts, getNewProducts } = require('./public/productDetailService');
+const { getAllProducts, searchProducts } = require('./public/productSearchService.js');
+const { getProductsByLocation } = require('./public/productLocationService.js');
+const { getCategories, getProductsByCategory } = require('./public/productCategoryService.js');
+const { getProductById, getFeaturedProducts, getNewProducts } = require('./public/productDetailService.js');
 
 module.exports = {
   getAllProducts,
