66c0bede64b097e46d2a441c54be3a67ed741556
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-12-15 11:49:26 +0000

    chore: Met à jour BlogPage et BlogDetailPage (contenu et SEO)

--- frontend/src/pages/BlogDetailPage.jsx
+++ frontend/src/pages/BlogDetailPage.jsx
@@ -269,7 +269,7 @@ const BlogDetailPage = () => {
           handleShare={handleShare}
         />
 
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {/* Contenu principal */}
             <div className="lg:col-span-2">
